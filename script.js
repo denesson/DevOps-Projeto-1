ceb1adb8de6bbc6128dc623ab52d6ac2b5f719b6
Denesson Barreto <denessonbarretodeoliveira@hotmail.com>
2024-05-08 19:29:30 -0300

    Adicionando arquivos do projeto

---
+++ script.js
@@ -0,0 +1,12 @@
+function mudar() {
+    // obitendo o elemento do corpo da página
+    var body = document.body;
+    // Definindo o novo background
+    //body.style.background = "red"
+
+    if(body){
+        alert("Ola, Denesson")
+    } else {
+        alert("fail......")
+    }
+}
